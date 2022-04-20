07901343a60382ef61bf2fd3068088be8ec568c4
Sergey Bebyakin <bambucia222@gmail.com>
2022-04-20 20:54:29 +0300

    1-st task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
-}
+  let result = 1;
+  while(n) {
+    result *= (n--); 
+    
+    }
+    console.log(result);
+    return(result);
+  }
+
+  factorial(0); 
+  factorial(1); 
+  factorial(3); 
+  factorial(5); 
+ 
