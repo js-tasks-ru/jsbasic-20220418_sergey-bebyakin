b5b59752f51aa7f288d28ce1cdb614d0ebbb1fcc
Sergey Bebyakin <bambucia222@gmail.com>
2022-05-01 07:37:03 +0300

    были реализованы 3 задачи 2-ого модуля.

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,19 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
+
+function sumSalary(salaries) {
+  let sum = 0;
+  for (let value of Object.values(salaries)) {
+      if (typeof value === 'number' && isFinite(value)) {
+        sum += value;
+      }      
+  }
+return sum;
+}
+sumSalary(salaries);
